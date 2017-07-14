15f1df6395765971e195c62d2175fa178ee973e8
choaticmorning <jvhoev@gmail.com>
2017-07-14 11:27:33 +0200

    firstapp commit

--- src/main/webapp/zoek.js
+++ src/main/webapp/zoek.js
@@ -41,10 +41,10 @@ function opslaan(){
 			}
 			$("#vhead").empty();
 			$("#vbody").empty();
-			$("#vhead").append("<tr><th>Naam</th><th>Plaats</th><th>Postcode</th><th>Linkedin</th><th>Email</th><th>Minimumloon</th></tr>")
+			$("#vhead").append("<tr><th>Naam</th><th>Plaats</th><th>Postcode</th><th>Minimumloon</th></tr>")
 			$.each(data, function(i, dat) {
 				codes = '"' + dat.id + '"'
-				$("#vbody").append("<tr id='info' class='tr' onclick='getInterimmer("+ codes +")'><td id='bedrijf' class='td1'>" + dat.voornaam +" "+ dat.achternaam + "</td><td id='plaats' class='td2'>"+ dat.woonplaats +"</td><td id='postcode' class='td3'>"+ dat.postcode +"</td><td id='link' class='td4'><a href='"+ dat.linkedin +"'>"+ dat.linkedin +"</a></td><td id='email' class='td5'>"+ dat.email +"</td><td id='mini' class='td6'>"+ dat.minimumloon +"</td></tr>");
+				$("#vbody").append("<tr id='info' class='tr' onclick='getInterimmer("+ codes +")'><td id='bedrijf' class='td1'>" + dat.voornaam +" "+ dat.achternaam + "</td><td id='plaats' class='td2'>"+ dat.woonplaats +"</td><td id='postcode' class='td3'>"+ dat.postcode +"</td><td id='mini' class='td6'>€"+ dat.minimumloon +"</td></tr>");
 			});
 				},
 		
